3ae3d258257919ab526a022224998ba119431cea
Hassan <mhas4908@gmail.com>
2021-03-05 19:47:57 +0500

    add a single comment in backend

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Comment
 
 class BlogAdmin(admin.ModelAdmin):
 	search_fields = ['title', 'description']
 	prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
 
