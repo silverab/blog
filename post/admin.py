db0502ccbaba22035b9a59f9ad67580b2193397b
Hassan <mhas4908@gmail.com>
2021-03-05 20:57:25 +0500

    add a comment code

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog, Comment
+from .models import Blog, Comment 
 
 class BlogAdmin(admin.ModelAdmin):
 	search_fields = ['title', 'description']
